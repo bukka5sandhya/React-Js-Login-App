6b10a40db425f38903e66877c3a725208443aa3c
bukka5sandhya <bukkapatnamsandhya920@gmail.com>
2024-03-25 18:30:56 +0530

    solution

---
+++ login-app/src/components/LogOut/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+const Logout = props => {
+    const {logout} = props
+
+    return(
+        <button className="logout-button" type="button" onClick ={logout}>Logout</button>
+    )
+
+}
+export default Logout;
---
+++ login-app/src/components/Login/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Login = props =>{
+    const {login} = props
+    return(
+        <button type="button" className="login-button" onClick={login}>Login</button>
+    )
+}
+export default Login;
